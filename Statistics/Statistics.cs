4b25b7934ee285b02e47a67dcb0d11b5d0b3e4fb
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-11 12:13:39 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,29 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public Stats CalculateStatistics(List<double> numbers) {
+           struct mysats
+           {
+               double average;
+               double max;
+               double min;
+           }
+           
+           mystats mystatsobj;
+           mystatsobj.average = Double.NaN;
+           mystatsobj.max = Double.NaN;
+           mystatsobj.min = Double.NaN;
+            
+           double my_avg = Queryable.Average(numbers.AsQueryable());
+           double my_max = numbers.Max();
+           double my_min = numbers.Min();
+           
+            mystatsobj.average = my_avg;
+            mystatsobj.max = my_max;
+            mystatsobj.min = my_min;
+            
+            return mystatsobj;
+           
         }
     }
 }
